c6593dc78ed619a39ea825ceb45fc8754d6a2827
Development Team <dev@digitalspider.com.au>
2024-05-04 01:16:11 +1000

    Fixed typo

--- api/src/services/aws/s3Service.js
+++ api/src/services/aws/s3Service.js
@@ -59,5 +59,5 @@ export async function getObjectToBuffer(Bucket, Key) {
     stream.on('data', (chunk) => chunks.push(chunk));
     stream.once('end', () => resolve(Buffer.concat(chunks)));
     stream.once('error', reject);
-  });
+  }));
 }
